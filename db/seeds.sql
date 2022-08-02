05443b15633e2e5618c9545b7a13eece1010e259
Aditya <90451018+realAdif@users.noreply.github.com>
2022-08-02 15:31:38 +1000

    updated SQL

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department(id, name)
+VALUES  (3,"Rick"),
+        (2,"Morty");
+
+INSERT INTO role(id,title,salary)
+VALUES  (7,"computer science",12.00),
+        (4,"AI Engineer",22.0);
+
+INSERT INTO employee(id,first_name,last_name)
+VALUES  (1,"Rick","Smith"),
+        (4,"Morty","Smith");
