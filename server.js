8cb3f1c8481344d57713567ea8f0a0c211f73abf
Aditya <90451018+realAdif@users.noreply.github.com>
2022-08-03 00:27:11 +1000

    Added .gitignore and a few functions

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
-
+require('console.table');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -22,6 +22,7 @@ const db = mysql.createConnection(
 
 prompt();
 
+//view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 function prompt(){
     inquirer.prompt([
         {
@@ -32,46 +33,80 @@ function prompt(){
                     'View all roles',
                     'View all employess',
                     'Add department',
-                    'Add a department',
+                    'Add a role',
+                    'Add a employee',
                     'Update an employee role',
                     'Exit'],
     
         },
     ]).then(({answers}) =>{
-        console.log(answers);
+        // console.log(answers);
         if(answers === 'View all department'){
-            console.log('this works');
-            prompt()
+            SQLdepartment();      
         }else if(answers === 'View all roles'){
-            SQLroles();
+            SQLroles();       
         }else if(answers === 'View all employess'){
-
+            SQLemployee();
         }else if(answers === 'Add department'){
+            SQLadddepartment()
+        }else if(answers === 'Add a role'){
 
-        }else if(answers === 'Add a department'){
+        }else if(answers === 'Add a employee'){
 
         }else if(answers === 'Update an employee role'){
 
         }
         else{
-            console.log("Bye");
+            console.log("Bye");   
         }
     });
 }
 
 function SQLroles(){
-    db.query('SELECT * FROM role', function (err, results) {
+    db.query('SELECT * FROM role',(err, results) =>{
         console.table(results);
+        prompt();   
+    });
+}
+function SQLdepartment(){
+    db.query('SELECT * FROM department',(err, results) =>{
+        console.table(results);  
+        prompt();  
+    });
+}
+function SQLemployee(){
+    db.query('SELECT * FROM employee',(err, results) =>{
+        console.table(results);  
+        prompt();  
     });
 }
 
+
+function SQLadddepartment(){
+    let AddDepartment;
+    inquirer.prompt({
+        type: 'input',
+        name: 'answers',
+        message: "What's the name of the department:",  
+    }).then(({answers}) =>{
+        console.log(answers);
+        AddDepartment = answers
+        db.query('INSERT INTO department(name) VALUES(?)',AddDepartment,(err, results) =>{
+            if (err) console.log(err);
+
+            console.log(results);
+            prompt();  
+        });
+    })
+    
+}
 
 app.use((req, res) => {
     res.status(404).end();
 });
   
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+   
 });
   
   
