3fd6c8fb1a3af3728c25091aa74b6f71685a102d
Aditya <90451018+realAdif@users.noreply.github.com>
2022-08-02 16:23:58 +1000

    fixed SQL

--- db/schema.sql
+++ db/schema.sql
@@ -3,28 +3,28 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 
-CREATE TABLE department(
-    id INT NOT NULL,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(id)
+CREATE TABLE department (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE role(
-    id INT,
-    title VARCHAR(30),
-    salary DECIMAL(7,2),
-    department_id INT,
-    FOREIGN KEY(department_id) REFERENCES department(id),
-    PRIMARY KEY(id) 
+CREATE TABLE role (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) UNIQUE NOT NULL,
+  salary DECIMAL UNSIGNED NOT NULL,
+  department_id INT UNSIGNED NOT NULL,
+  INDEX dep_ind (department_id),
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE employee(
-    id INT,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT,
-    FOREIGN KEY(id) REFERENCES role(id) ON DELETE SET NULL,
-    PRIMARY KEY(id),
-    FOREIGN KEY (id) REFERENCES employee(manager_id) ON DELETE SET NULL
+CREATE TABLE employee (
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT UNSIGNED NOT NULL,
+  INDEX role_ind (role_id),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+  manager_id INT UNSIGNED,
+  INDEX man_ind (manager_id),
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
